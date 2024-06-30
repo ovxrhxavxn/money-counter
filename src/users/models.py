4d331b32d51a0e71911dddbaa6bda6133f7ab1df
Ivan <evankirk@vk.com>
2024-06-30 19:02:32 +0500

    Добавил поле "role" как атрибут сущности

--- src/users/models.py
+++ src/users/models.py
@@ -1,8 +1,6 @@
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Integer, String, ForeignKey, Column
+from sqlalchemy import Integer, String, Column
 
-
-Base = declarative_base()
+from database.database import Base
 
 
 class User(Base):
@@ -10,21 +8,6 @@ class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
+    name = Column(String, nullable=False, unique=True)
+    role = Column(String, nullable=False)
     token_amount = Column(Integer, nullable=False)
-
-
-class Role(Base):
-
-    __tablename__ = 'roles'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String(15), nullable=False)
-
-
-class UserRole(Base):
-
-    __tablename__ = 'users_roles'
-
-    user_id = Column(Integer, ForeignKey('users.id'), primary_key=True)
-    role_id = Column(Integer, ForeignKey('roles.id'), primary_key=True)
