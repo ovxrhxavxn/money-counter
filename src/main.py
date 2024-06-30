3624b3d7636e4a15c489f1cccfe6257bcce3fc47
Ivan <evankirk@vk.com>
2024-06-30 19:01:31 +0500

    Добавил lifespan для проверки (создания) БД

--- src/main.py
+++ src/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
 from cv_models.router import router as cv_router
 from users.router import router as users_router
+from database.database import create_tables
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    
+    await create_tables()
+
+    yield   
 
 app = FastAPI(
-
+    lifespan=lifespan,
     title='Money Counter',
     summary='Backend для проекта Money Counter',
     version='1.0.0'
