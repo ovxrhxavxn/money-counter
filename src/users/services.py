ca3bf1d53abaa037621b397cb8bf05b849bbb24f
Ivan <evankirk@vk.com>
2024-09-21 18:46:22 +0500

    Отформатировал код

--- src/users/services.py
+++ src/users/services.py
@@ -11,7 +11,7 @@ class UserService:
 
     
     async def add(self, schema: UserSchema):
-
+    
         user_dict = schema.model_dump()
 
         await self._user_repo.add(user_dict)
@@ -30,7 +30,7 @@ class UserService:
     async def get_all(self):
 
         return await self._user_repo.get_all()
-    
+
 
     async def change_token_amount(self, user_name: str, new_value: int):
 
