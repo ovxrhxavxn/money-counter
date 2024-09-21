607e3cccc32ea7ec3c256ba6c84541e03f19ff5b
Ivan <evankirk@vk.com>
2024-09-21 18:22:38 +0500

    Сделал зависимость сервиса от абстрактных репозиториев

--- src/cv_models/services.py
+++ src/cv_models/services.py
@@ -1,22 +1,35 @@
-import io
 from pathlib import Path
+from io import BytesIO
+from typing import AsyncGenerator
 
 from fastapi import UploadFile
-from fastapi.exceptions import HTTPException
 from PIL.Image import Image
 
-from .repositories import CVModelRepository, TaskRepository
-from users.repositories import UserRepository
-from .schemas import TaskSchema, CVModelEnum
+from database.repositories import (
+
+    AbstractCVModelsRepository,
+    AbstractTasksRepository,
+    AbstractUserRepository
+)
+from .schemas import TaskSchema, CVModelEnum, TaskHistorySchema
+from background_tasks.dramatiq.tasks  import use_cv_model
+from cv_models.cv_processing.yolo8model import (
+
+    YOLO8Model,
+    YOLO8N,
+    YOLO8S,
+    YOLO8M
+)
+
 
 class CVModelsService:
 
     def __init__(
             
             self, 
-            cv_model_repo: type[CVModelRepository], 
-            task_repo: type[TaskRepository], 
-            user_repo: type[UserRepository]
+            cv_model_repo: type[AbstractCVModelsRepository], 
+            task_repo: type[AbstractTasksRepository], 
+            user_repo: type[AbstractUserRepository]
             
             ):
 
@@ -25,9 +38,9 @@ class CVModelsService:
         self._user_repo = user_repo()
 
 
-    async def _use(self, user_name: str, cv_model_name: CVModelEnum, image: UploadFile) -> dict | None:
+    async def _use_model(self, user_name: str, cv_model: YOLO8Model, image: bytes) -> AsyncGenerator:
 
-        cv_model_id = await self._cv_model_repo.get_id(cv_model_name)
+        cv_model_id = await self._cv_model_repo.get_id(cv_model.name)
 
         await self._task_repo.add(TaskSchema(cv_model_id=cv_model_id))
 
@@ -35,44 +48,66 @@ class CVModelsService:
 
         try:
 
-            await self._user_repo.get_by_name(user_name)
+            await self._user_repo.get(user_name)
 
             path = Path(f'database/images/{last_task.id}.jpeg').resolve()
 
-            Image().save(path, image.file.read())
+            bytes_io = BytesIO(image)
 
-            msg = "TasksSet.use_yolo8s.send(user_name, last_task.id, str(path))"
+            
+
+            msg = use_cv_model.send(cv_model, image)
 
             await self._task_repo.update_msg_id(last_task.id, msg.message_id)
+
+            yield {'task_id' : msg.message_id}
+
+            await self._task_repo.update_result(
+
+                    msg.msg_id, 
+                    str(Path(f'database/images/processed/{last_task.id}Result.jpeg'))
+                    
+                    )
+
+            await self._task_repo.update_result_sum(last_task.id, msg.get_result()[0])
+
+            cv_model_from_db = await self._cv_model_repo.get(cv_model.name)
+
+            await self._user_repo.subtract_from_token_amount(user_name, cv_model_from_db.cost)
+
+            user_id = await self._user_repo.get_id(user_name)
+
+            await self._task_repo.add(TaskHistorySchema(
+
+                user_id=user_id,
+                task_id=last_task.id
+            ))
 
         except IndexError:
 
             await self._task_repo.delete(last_task.id)
 
-            raise HTTPException(404, detail='The user doesn`t exist')
+            raise
         
         except Exception:
 
             await self._task_repo.delete(last_task.id)
-        
-        else:
-
-            return {'task_id' : msg.message_id}
+    
 
 
     async def use_yolo8s(self, user_name: str, image: UploadFile):
-
-        return await self._use(user_name, CVModelEnum.YOLO8S, image)
+        
+        await self._use_model(user_name, YOLO8S(), image.file.read())
 
 
     async def use_yolo8m(self, user_name: str, image: UploadFile):
 
-        return await self._use(user_name, CVModelEnum.YOLO8M, image)
+       yield await self._use_model(user_name, YOLO8M(), image.file.read())
 
 
     async def use_yolo8n(self, user_name: str, image: UploadFile):
 
-       return await self._use(user_name, CVModelEnum.YOLO8N, image)
+       yield await self._use_model(user_name, YOLO8N(), image.file.read())
     
 
     async def get(self, name: str):
@@ -83,3 +118,45 @@ class CVModelsService:
     async def change_cost(self, name: str, new_value: int):
 
         await self._cv_model_repo.change_cost(name, new_value)
+
+
+class TasksService:
+
+    def __init__(self, task_repo: type[AbstractTasksRepository]):
+
+        self._task_repo = task_repo()
+
+
+    async def get_by_msg_id(self, msg_id: str):
+
+        return await self._task_repo.get_by_msg_id(msg_id)
+    
+
+    async def update_result(self, msg_id: str, new_result: str):
+
+        await self._task_repo.update_result(msg_id, new_result)
+
+
+    async def get_last(self):
+
+        return await self._task_repo.get_last()
+    
+
+    async def update_msg_id(self, new_msg_id: str):
+
+        await self._task_repo.update_msg_id(new_msg_id)
+
+    
+    async def update_result_sum(self, id: int, new_sum: int):
+
+        await self._task_repo.update_result_sum(id, new_sum)
+
+
+    async def check_task_result(self, msg_id: str):
+
+        task = await self._task_repo.get_by_msg_id(msg_id)
+
+        if not task.image_id:
+            raise ValueError("Task result is None")
+        
+        return task
