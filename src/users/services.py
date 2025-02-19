d5e39d267bacec0b29cebd1395144d0a72cf0ae0
ovxrhxavxn <evankirk@vk.com>
2025-02-20 01:14:25 +0500

    Вынести приведение объекта  БД к схеме в сервис

--- src/users/services.py
+++ src/users/services.py
@@ -1,42 +1,43 @@
 from roles.schemas import Role
-from .repositories import UserRepository
-from .schemas import UserSchema
+from database.repositories import AbstractUserRepository
+from .schemas import UserDate, User
 
 
 class UserService:
 
-    def __init__(self, user_repo: type[UserRepository]) -> None:
-
+    def __init__(self, user_repo: type[AbstractUserRepository]) -> None:
         self._user_repo = user_repo()
 
     
-    async def add(self, schema: UserSchema):
-    
+    async def add(self, schema: User):
         user_dict = schema.model_dump()
 
         await self._user_repo.add(user_dict)
 
         
     async def delete(self, id: int):
-
         await self._user_repo.delete(id)
     
     
-    async def get_by_name(self, name: str) -> UserSchema:
+    async def get_by_name(self, name: str) -> UserDate:
+        user = await self._user_repo.get(name)
 
-        return await self._user_repo.get(name)
+        return UserDate.model_validate(user)
     
 
     async def get_all(self):
+        rows = await self._user_repo.get_all()
 
-        return await self._user_repo.get_all()
+        return [UserDate.model_validate(row[0]) for row in rows]
 
 
     async def change_token_amount(self, user_name: str, new_value: int):
-
         await self._user_repo.change_token_amount(user_name, new_value)
 
 
     async def change_role(self, user_name: str, new_role: Role):
-
         await self._user_repo.change_role(user_name, new_role)
+
+
+    async def get_id(self, name: str) -> int:
+        await self._user_repo.get_id(name)
