3a910ee012d19326b46dc6a47de1146e0db98975
ovxrhxavxn <evankirk@vk.com>
2025-02-20 13:49:49 +0500

    Реализовать алгоритм использования модели

--- src/cv_models/services.py
+++ src/cv_models/services.py
@@ -1,17 +1,16 @@
 from pathlib import Path
+from PIL import Image
 
 import dramatiq
-from dramatiq import Message
-from dramatiq.results.errors import ResultMissing
 
 from database.repositories import (
 
     AbstractCVModelRepository,
     AbstractTaskRepository,
-    AbstractUserRepository
+    AbstractUserRepository,
+    AbstractImageRepository
 )
-from .schemas import TaskSchema, TaskHistorySchema
-from background_tasks.tasks  import use_cv_model
+from .schemas import TaskSchema, TaskId, TaskHistorySchema
 from cv_models.core.yolo8model import (
 
     YOLO8Model,
@@ -19,6 +18,7 @@ from cv_models.core.yolo8model import (
     YOLO8S,
     YOLO8M
 )
+from images.schemas import ImageSchema
 
 
 class CVModelsService:
@@ -28,32 +28,112 @@ class CVModelsService:
             self, 
             cv_model_repo: type[AbstractCVModelRepository], 
             task_repo: type[AbstractTaskRepository], 
-            user_repo: type[AbstractUserRepository]
+            user_repo: type[AbstractUserRepository],
+            image_repo: type[AbstractImageRepository]
             
             ):
 
         self._cv_model_repo = cv_model_repo()
         self._task_repo = task_repo()
         self._user_repo = user_repo()
+        self._image_repo = image_repo()
 
 
-    async def _use_model(self, cv_model: YOLO8Model, image: bytes):
+    @dramatiq.actor
+    @dramatiq.asyncio.async_to_sync
+    async def _use_cv_model(
 
-        # TODO: Реализовать бизнес-логику использования модели
+            self, 
+            cv_model: YOLO8Model, 
+            image: bytes, 
+            task_id: int, 
+            user_name: str
+        
+        ):
 
-        msg = use_cv_model.send(cv_model, image)
+        sum, _, _ = cv_model.use(image)
+
+        task: TaskId = await self._task_repo.get(task_id)
+
+        await self._task_repo.update_result(task.msg_id, str(Path(f'images/processed/{task_id}Result.jpeg')))
+
+        await self._task_repo.update_result_sum(task_id, sum)
+
+        await self._user_repo.subtract_from_token_amount(user_name, cv_model.cost)
+
+        user_id = await self._user_repo.get_id(user_name)
+
+        await self._task_repo.add(TaskHistorySchema(
+
+                user_id=user_id,
+                task_id=task_id
+        
+            ).model_dump()
+        )
+
+
+    async def _set_cv_model_task(self, user_name: str, cv_model: YOLO8Model, image: bytes):
+
+        cv_model_id = await self._cv_model_repo.get_id(cv_model.name)
+
+        await self._task_repo.add(
+
+            TaskSchema(
+
+                cv_model_id=cv_model_id, 
+                msg_id=None, 
+                result_sum=None
+
+            ).model_dump()
+        )
+        
+        last_task = await self._task_repo.get_last()
+
+        try:
+
+            await self._user_repo.get(user_name)
+
+            path = str(Path(f'images/{last_task.id}.jpeg'))
+
+            Image.open(path).save(image)
+
+            ImageSchema(
+
+                path = path
+
+            ).model_dump()
+
+            msg = self._use_cv_model.send(self, cv_model, image, last_task.id, user_name)
+
+            await self._task_repo.update_msg_id(last_task.id, msg.message_id)
+
+        except IndexError:
+
+            await self._task_repo.delete(last_task.id)
+
+            raise
+        
+        except Exception:
+
+            await self._task_repo.delete(last_task.id)
+
+            raise
+        
+        else:
+
+            return {'task_id' : msg.message_id}
         
     
     async def use_yolo8s(self, user_name: str, image: bytes):
-        return await self._use_model(user_name, YOLO8S(), image)
+        return await self._set_cv_model_task(user_name, YOLO8S(), image)
 
 
     async def use_yolo8m(self, user_name: str, image: bytes):
-       return await self._use_model(user_name, YOLO8M(), image)
+       return await self._set_cv_model_task(user_name, YOLO8M(), image)
 
 
     async def use_yolo8n(self, user_name: str, image: bytes):
-       return await self._use_model(user_name, YOLO8N(), image)
+       return await self._set_cv_model_task(user_name, YOLO8N(), image)
     
 
     async def get(self, name: str):
@@ -62,10 +142,6 @@ class CVModelsService:
     
     async def change_cost(self, name: str, new_value: int):
         await self._cv_model_repo.change_cost(name, new_value)
-
-
-    async def fill_table(self):
-        await self._cv_model_repo.fill_table()
 
 
 class TasksService:
