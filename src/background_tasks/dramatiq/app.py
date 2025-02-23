4753559c6b2e1840e0d9a0145bbc6a21c7e5586d
ovxrhxavxn <evankirk@vk.com>
2025-02-23 14:56:57 +0500

    Отформатировать код

--- src/background_tasks/dramatiq/app.py
+++ src/background_tasks/dramatiq/app.py
@@ -2,6 +2,7 @@ import dramatiq
 from dramatiq.brokers.redis import RedisBroker
 from dramatiq.middleware import AsyncIO
 
+
 broker = RedisBroker(url='redis://localhost:6379')
 broker.add_middleware(AsyncIO())
 
