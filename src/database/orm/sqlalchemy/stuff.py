7f2dfafe20036985cdb957914801ba508f4c4608
ovxrhxavxn <evankirk@vk.com>
2025-02-23 14:48:20 +0500

    Объединить импорты

--- src/database/orm/sqlalchemy/stuff.py
+++ src/database/orm/sqlalchemy/stuff.py
@@ -1,6 +1,10 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import (
+    
+    DeclarativeBase,
+    sessionmaker
+)
+
 from ...config import config
 
 
